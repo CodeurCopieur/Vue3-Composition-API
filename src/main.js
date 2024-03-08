54b8c6a4ab87fb51e5f17ae4c1fa2881225876d0
Codwerk <36073831+CodeurCopieur@users.noreply.github.com>
2024-03-08 10:32:15 +0100

    Comprendre l'instance d'application createApp dans le projet Vue

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import MyComponents from './components/MyComponents.vue'
 
-createApp(App).moun*t('#app')
+const app = createApp(App)
+app.component('MyComponents', MyComponents)
+app.mount('#app')
+
+const app2 = createApp(App)
+app2.mount('#app2')
