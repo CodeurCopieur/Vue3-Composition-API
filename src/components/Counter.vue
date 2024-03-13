7cf9c33da584dbb5619085f192661538ebf9384b
Codwerk <36073831+CodeurCopieur@users.noreply.github.com>
2024-03-13 20:53:29 +0100

    Implémentez le composant compteur avec le bouton d'incrémentation et de décrémentation en utilisant

---
+++ src/components/Counter.vue
@@ -0,0 +1,30 @@
+<script setup>
+  import { ref } from 'vue'
+
+  const counter = ref(0);
+
+  const increase = () => {
+    counter.value++;
+  };
+  const decrease = () => {
+    counter.value--;
+  };
+</script>
+
+<template>
+ <div class="center">
+  <h1>Counter Component</h1>
+ <div>
+  <button @click.prevent="decrease()" v-if="counter.value < 0">-</button>
+  &nbsp;
+  <span>{{counter}}</span>
+  &nbsp;
+  <button @click.prevent="increase()">+</button>
+ </div>
+ </div>
+</template>
+<style scoped>
+  .center {
+    text-align: center;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import MyComponents from './components/MyComponents.vue'
+import Counter from './components/Counter.vue'
 
 const app = createApp(App)
 app.component('MyComponents', MyComponents)
+app.component('Counter', Counter)
 app.mount('#app')
 
-const app2 = createApp(App)
-app2.mount('#app2')
+// const app2 = createApp(App)
+// app2.mount('#app2')
