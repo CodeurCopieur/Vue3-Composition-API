4dc6014f0da7aa2fe908fb3eca9023333d626336
Codwerk <36073831+CodeurCopieur@users.noreply.github.com>
2024-03-15 13:54:49 +0100

    Comprendre la syntaxe des modèles basés sur HTML dans l'API de composition dans Vue

--- src/components/TemplateSyntax.vue
+++ src/components/TemplateSyntax.vue
@@ -1,11 +1,24 @@
 <script setup>
-  
+  const dynamicId = 'my-id'
+  const message = '<h1>Bonjour</h1>'
+  const objectOfAttrs = {
+    id: dynamicId,
+    class: 'container'
+  };
+
+  const titleAttr = "title";
+  const titleValue = "C'est est un titre"
+  const eventName = 'mouseover';
+
+  const changeTitle = ()=> {
+    alert('changeTitle')
+  };
 </script>
 
 <template>
- 
+  <h1>Template Component</h1>
+ <div :id="dynamicId" :[titleAttr] = "titleValue">Template Code</div>
+ <div v-bind="objectOfAttrs">Test objectOfAttrs</div>
+ <div v-html="message" @[eventName]="changeTitle()"></div>
+ <p @click="changeTitle()">{{ message ? 'Yes' : 'No' }}</p>
 </template>
-
-<style lang="scss" scoped>
-  
-</style>
