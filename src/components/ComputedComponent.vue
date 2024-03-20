6415c757385a42a8e9bc3c5e14e07326e66c8b82
Codwerk <36073831+CodeurCopieur@users.noreply.github.com>
2024-03-20 23:12:25 +0100

    Computed Property

---
+++ src/components/ComputedComponent.vue
@@ -0,0 +1,31 @@
+<script setup>
+  import { computed, reactive, ref } from 'vue';
+
+  const employees = reactive(['emp1', 'emp2']);
+
+  const isEmployeesPresent = computed( () => employees.length > 0 ? 'Yes computed' : 'No');
+
+  const firstName = ref('Codwerk');
+  const lastName = ref('Dev Front');
+
+  const fullName = computed(()=> firstName.value  + " " + lastName.value);
+
+  const fullName1 = computed({
+    get() {
+      return firstName.value  + " " + lastName.value
+    },
+    set(newValue){
+      [firstName.value ,lastName.value] = newValue.split(" ");
+    }
+  });
+
+  fullName1.value = 'Hi Boss'
+
+</script>
+
+<template>
+ <h2>Computed Property </h2>  
+ <p>Employees Present : {{ isEmployeesPresent }}</p>
+ <p>fullName : {{ fullName }}</p>
+ <p>fullName1 : {{ fullName1 }}</p>
+</template>
--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,5 +1,5 @@
 <script setup>
-  import { reactive, ref } from 'vue'
+  import { computed, reactive, ref } from 'vue'
 
   const counter = ref(0);
 
@@ -16,20 +16,23 @@
     counter: 0,
     name: 'Template Component',
   });
+
+  const pairOrimpair = computed(()=> { return counterData.counter % 2 === 0 ? 'Pair' : 'Impair' });
 </script>
 
 <template>
  <div class="center">
-  <h2>ref : {{ NameTemplate }} <br> reactive : {{counterData.name}}</h2>
- <div>
-  <button @click.prevent="decrease()">-</button>
-  &nbsp;
-  <span>{{counterData.counter}}</span>
-  &nbsp;
-  <button @click.prevent="increase()">+</button>
- </div>
-
-  <input type="text" v-model="counterData.name" name="" id="">
+    <h2>ref : {{ NameTemplate }} <br> reactive : {{counterData.name}}</h2>
+    <div>
+      <button @click.prevent="decrease()">-</button>
+      &nbsp;
+      <span>{{counterData.counter}}</span>
+      &nbsp;
+      <button @click.prevent="increase()">+</button>
+    </div>
+    <hr />
+    <p>La valeur du counter is {{ pairOrimpair }}</p>
+    <input type="text" v-model="counterData.name" name="" id="">
  </div>
 </template>
 <style scoped>
