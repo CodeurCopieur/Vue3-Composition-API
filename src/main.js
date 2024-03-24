aff53cdf0af0d00b7ac0b56ef9399ec96ba2f9f3
Codwerk <36073831+CodeurCopieur@users.noreply.github.com>
2024-03-24 17:55:53 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import MyComponents from './components/MyComponents.vue'
+import ClassComponent from './components/ClassComponent.vue'
 import ComputedComponent from './components/ComputedComponent.vue'
 import TemplateSyntax from './components/TemplateSyntax.vue'
 import Counter from './components/Counter.vue'
@@ -11,6 +12,7 @@ app.component('MyComponents', MyComponents)
 app.component('Counter', Counter)
 app.component('TemplateSyntax', TemplateSyntax)
 app.component('ComputedComponent', ComputedComponent)
+app.component('ClassComponent', ClassComponent)
 app.mount('#app')
 
 // const app2 = createApp(App)
