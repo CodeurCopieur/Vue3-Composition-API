a39ad7e441fbc07896502adb55febf2a3ade665a
Codwerk <36073831+CodeurCopieur@users.noreply.github.com>
2024-03-24 21:29:26 +0100

    Liaisons de classe dans l'objet API de composition Vue 3 et Liaisons de classe de tableau

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,12 @@
    <h1>Vue 3 Composition API</h1>
 
    <my-components :msg="'lol'"></my-components>
+   <hr>
    <Counter></Counter>
+   <hr>
    <TemplateSyntax></TemplateSyntax>
+   <hr>
    <ComputedComponent></ComputedComponent>
+   <hr>
+   <ClassComponent class="ClassComponent"></ClassComponent>
 </template>
